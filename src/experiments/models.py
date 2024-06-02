8e5dd72ec20fef8828d8f2ab3874c39ff6e5ac31
Daniel Zdancewicz <inwazzin@gmail.com>
2024-06-02 20:13:00 +0200

    run

--- src/experiments/models.py
+++ src/experiments/models.py
@@ -1,6 +1,9 @@
 from typing import Protocol, Callable, Mapping
 
 from pandas import DataFrame, Series
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC
 
 class Model(Protocol):
@@ -12,9 +15,9 @@ class Model(Protocol):
 
 def create_model_descriptors() -> Mapping[str, Callable[[], Model]]: return {
   "SVC": lambda: SVC(),
-  # "KNN2": lambda: KNeighborsClassifier(n_neighbors=2),
-  # "KNN3": lambda: KNeighborsClassifier(n_neighbors=3),
-  # "KNN4": lambda: KNeighborsClassifier(n_neighbors=4),
-  # "RandomForest": lambda: RandomForestClassifier(max_depth=8),
-  # "MLP": lambda: MLPClassifier(max_iter=100_000),
+  "KNN2": lambda: KNeighborsClassifier(n_neighbors=2),
+  "KNN3": lambda: KNeighborsClassifier(n_neighbors=3),
+  "KNN4": lambda: KNeighborsClassifier(n_neighbors=4),
+  "RandomForest": lambda: RandomForestClassifier(max_depth=8),
+  "MLP": lambda: MLPClassifier(max_iter=100_000),
 }
